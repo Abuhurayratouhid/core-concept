c9398166ca6131efc3e8ae1006ed493f7dfa9731
Abuhurayratouhid <abuhurayratouhid@gmail.com>
2022-09-20 16:41:14 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
-
+const singer = {id : 1, Name:"mahfuz", job:'singer'}
+const containerStyle = {
+  color: 'black',
+  backgroundColor: 'red' ,
+  padding:"30px"
+};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person name="Masrafi"></Person>
+      <Person name="Musfiq"></Person>
+      <Person name="rubel"></Person>
+      <h1>Section two </h1>
+      <Nayok name="shahrukh khan" movie="ddlj"></Nayok>
+      <Nayok></Nayok>
+      <Nayok></Nayok>
     </div>
   );
+}
+
+function Person(Props){
+  return (
+  <div className='person'>
+    <h1>Name:{Props.name}</h1>
+    <h1>profesion:creketer </h1>
+    <h1>id:1 </h1>
+  </div>
+  ) 
+ 
+}
+
+function Nayok(props){
+  return (
+    <div className='nayok'>
+      <h1>{props.name}</h1>
+      <h1>{props.movie} </h1>
+      <h1>id:2 </h1>
+    </div>
+    )
 }
 
 export default App;
